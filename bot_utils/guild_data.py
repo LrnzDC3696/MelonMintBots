c110c34639c52f47ec22bc163311df479f204528
Xzlaynveir-Deirdre <xzlayn.dre@gmail.com>
2021-02-16 11:45:14 +0800

    for the rules stuff

--- bot_utils/guild_data.py
+++ bot_utils/guild_data.py
@@ -1,11 +1,15 @@
-from hata import Guild, ChannelText, Role
+from hata import Guild, ChannelText, ChannelVoice,Role
 
 #Guild
 NIHONGO_QUEST = Guild.precreate(709105396656111676)
 
 #Channel
 WELCOME_CHANNEL = ChannelText.precreate(809349050091569152)
-MEMBER_COUNT = 809262218763304960
+MEMBER_COUNT = ChannelVoice.precreate(809262218763304960)
+
+#Bot related channels
+BOT_LOGS = ChannelText.precreate(811036715992612864)
+DM_BOT_SUPPORT = ChannelText.precreate(811036609809612830)
 
 #Roles
 MUTED = Role.precreate(809443793413603409)
@@ -23,7 +27,7 @@ FREE_ROLES = {
 
 #Rules
 SERVER_RULES = {
-    "1":"Please follow [discord's term of use]()",
+    "1":"Please follow [discord's term of use](https://discord.com/terms)",
     "2":"Be Nice and have fun let's be an encouraging server.",
     "3":"Do not post anything illegal or any ip grabber links.",
     "4":"Please do not disclose your personal information to anyone.",
@@ -31,8 +35,9 @@ SERVER_RULES = {
     "6":"No spamming or anything if the mods told you to stop please stop.",
     "7":"Please respect others at all times. Harassment, dogpiling, etc. are not cool.",
     "8":"If someone is messaging you things you do not like please report it to the mods.",
-    "9":"Please avoid potentionally offensive conversationm, such as politics, racism and religion.",
-    "10":"Be humble. Our server is built up of people regardless of their level. If you notice someone is new please help them"
+    "9":"Please avoid potentionally offensive conversation, such as politics, racism and religion.",
+    "10":"Be humble. Our server is built up of people regardless of their level. If you notice someone is new please help them",
+    "11":"Please use the correct the channel according to their topic"
     }
 
 #Nihongo Quest Links
@@ -42,6 +47,6 @@ LINKS = {
     'FACEBOOK':'https://facebook.com/NihongoQuest/',
     'TWITTER':'https://twitter.com/bailey_eng',
     'INSTAGRAM':'https://www.instagram.com/nihongoquestgame/',
-    'ITCH':'https://melonmintgames.itch.io/nihongo-quest',
+    'ITCH':'https://itch.io',
     'DISCORD':'https://discord.gg/B5WXZdj',
     }
--- modules/server.py
+++ modules/server.py
@@ -1,6 +1,6 @@
 from bot_utils.guild_data import SERVER_RULES, WELCOME_CHANNEL
 
-from hata import Client, sleep
+from hata import Client, sleep, Embed
 
 MMB: Client
 
@@ -8,15 +8,14 @@ MMB: Client
 
 # every 1 hour Send message to start convo event
 
-# Dm Suggestion
-# Dm report
-
 # Rules
 # Embedify this;
 @MMB.commands
 async def rule(client, message, rule_number):
     the_rule = SERVER_RULES.get(rule_number)
-    await client.message_create(message, the_rule if the_rule else 'Rule Not Found')
+    embed = Embed(color=0x1489B0)
+    embed.add_field(f'RULE {rule_number}', the_rule)
+    await client.message_create(message.channel, embed)
 
 # Welcomes new users
 # Direct Message this
@@ -24,6 +23,11 @@ async def rule(client, message, rule_number):
 async def guild_user_add(client, _ , user):
     if user.is_bot:
         return
+    
+    #In DM
+    
+    
+    #In Channel
     message = f'Welcome {user:m} please check <#717221907480576000> and <#717220941519781900> for more info on our current situation'
     to_send = await client.message_create(WELCOME_CHANNEL, message)
     await sleep(60*3)
